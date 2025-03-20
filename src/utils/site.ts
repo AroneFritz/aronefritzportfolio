20f25acdc177cb3847b89b3a2e6e80c1e0020dc3
Fritzkol <aronelamanilao5@gmail.com>
2025-03-21 00:47:47 +0800

    Initial portfolio commit

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,16 +1,15 @@
 const site_url =
-  process.env.NEXT_PUBLIC_APP_URL || "https://arone-portfolio.netlify.app";
+  process.env.NEXT_PUBLIC_APP_URL || "https://the-portfolio-lac.vercel.app";
 
 export const siteConfig = {
   name: "Arone Fritz Lamanilao | Web Stack Developer",
   description:
-    "Professional portfolio website showcasing my projects and skills as a Web Stack Developer. Explore my latest work, technical expertise, and professional experience.",
+    "Personal portfolio website showcasing my projects and skills as a Web Stack Developer",
   url: site_url,
-  ogImage: `${site_url}/Arone-Fritz.png`,
+  ogImage: `${site_url}/_static/og-image.png`,
   links: {
     twitter: "https://twitter.com/aronefritz",
-    github: "https://github.com/AroneFritz/AroneFritz-Portfolio",
-    linkedin: "https://www.linkedin.com/in/arone-fritz-lamanilao"
+    github: "https://github.com/AroneFritz/main-portfolio",
   },
   mailSupport: "aronefritz@gmail.com",
 };
