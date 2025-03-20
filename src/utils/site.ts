667d02b4117a9ca42b03f650597dba7054ed3da8
Fritzkol <aronelamanilao5@gmail.com>
2025-03-21 01:22:50 +0800

    Add strong timestamp to force social media cache refresh

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,12 +1,15 @@
 const site_url =
   process.env.NEXT_PUBLIC_APP_URL || "https://aronefritzportfoliov2.netlify.app";
 
+// Add a fixed timestamp to force cache refreshing
+const timestamp = "20240321" + Math.floor(Math.random() * 1000);
+
 export const siteConfig = {
   name: "Arone Fritz Lamanilao | Web Stack Developer",
   description:
     "Personal portfolio website showcasing my projects and skills as a Web Stack Developer",
   url: site_url,
-  ogImage: `${site_url}/og-image.png?v=${new Date().getTime()}`,
+  ogImage: `${site_url}/og-image.png?v=${timestamp}`,
   links: {
     twitter: "https://twitter.com/aronefritz",
     github: "https://github.com/AroneFritz/aronefritzportfolio",
