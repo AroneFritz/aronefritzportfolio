9aa758a39c63bc2ddb9a9afd00d90ec946e7d171
Fritzkol <aronelamanilao5@gmail.com>
2025-03-21 00:57:20 +0800

    Update site configuration for Open Graph image preview on social media

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,15 +1,15 @@
 const site_url =
-  process.env.NEXT_PUBLIC_APP_URL || "https://the-portfolio-lac.vercel.app";
+  process.env.NEXT_PUBLIC_APP_URL || "https://aronefritzportfolio.netlify.app";
 
 export const siteConfig = {
   name: "Arone Fritz Lamanilao | Web Stack Developer",
   description:
     "Personal portfolio website showcasing my projects and skills as a Web Stack Developer",
   url: site_url,
-  ogImage: `${site_url}/_static/og-image.png`,
+  ogImage: `${site_url}/Arone-Fritz.png`,
   links: {
     twitter: "https://twitter.com/aronefritz",
-    github: "https://github.com/AroneFritz/main-portfolio",
+    github: "https://github.com/AroneFritz/aronefritzportfolio",
   },
   mailSupport: "aronefritz@gmail.com",
 };
